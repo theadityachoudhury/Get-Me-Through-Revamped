e59dbf5c3a247266d15cbeb897005ac1c027b0ab
Aditya Choudhury <adityasubham03@gmail.com>
2023-12-21 23:00:46 +0530

    Initial project setup

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({
+	subsets: ["latin"],
+	weight: ["400", "500", "600", "700"],
+	variable: "--font-poppins",
+});
+
+export const metadata: Metadata = {
+	title: "Get Me Through",
+	description: "Get Me Through is a platform for event management",
+};
+
+export default function RootLayout({
+	children,
+}: {
+	children: React.ReactNode;
+}) {
+	return (
+		<html lang="en">
+			<body className={poppins.variable}>{children}</body>
+		</html>
+	);
+}
