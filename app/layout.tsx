621093333a5ee9e03f5d8a22791105b297da8e02
Aditya Choudhury <adityasubham03@gmail.com>
2023-12-21 23:51:22 +0530

    Added header and authentication using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
 	subsets: ["latin"],
@@ -11,6 +12,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
 	title: "Get Me Through",
 	description: "Get Me Through is a platform for event management",
+	icons: "/assets/images/logo.svg",
 };
 
 export default function RootLayout({
@@ -19,8 +21,10 @@ export default function RootLayout({
 	children: React.ReactNode;
 }) {
 	return (
-		<html lang="en">
-			<body className={poppins.variable}>{children}</body>
-		</html>
+		<ClerkProvider>
+			<html lang="en">
+				<body className={poppins.variable}>{children}</body>
+			</html>
+		</ClerkProvider>
 	);
 }
